0d8afbe094a94169503b0cf2c8f945f43322e84d
annonymouse007 <43579453+@users.noreply.github.com>
2019-01-28 13:27:04 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -57,7 +57,7 @@ include ("auth.php");
                     </li>
                    
                     <li>
-                        <a class="nav-link" href="contact.php">
+                        <a class="nav-link" href="contactadmin.html">
                             <i class="nc-icon nc-bell-55"></i>
                             <p>Contact Admin</p>
                         </a>
