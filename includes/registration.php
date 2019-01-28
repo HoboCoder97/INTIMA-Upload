99e37f4368464836f02e8bf17d9403776d38f2c9
HoboCoder97 <45557746+HoboCoder97@users.noreply.github.com>
2019-01-28 18:00:04 +0800

    Added Uploads

--- includes/registration.php
+++ includes/registration.php
@@ -10,15 +10,27 @@ if (isset($_REQUEST['username'])){
 	$email = mysqli_real_escape_string($con,$email);
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($con,$password);
-
+    $checkquery = "SELECT * FROM `users` WHERE username = 'username'";
+    $checkresult = mysqli_query ($con, $checkquery);
+    echo $checkresult;
+    if (!checkresult){
         $query = "INSERT into `users` (username, password, email)
 VALUES ('$username', '".md5($password)."', '$email')";
         $result = mysqli_query($con,$query);
+     
         if($result){
             echo "<div class='form'>
 <h3>You are registered successfully.</h3>
 <br/>Click here to <a href='index.php'>Login</a></div>";
+        
         }
-    }else{
+    else{
+            }
+    }
+    else {
+    echo "<script type='text/javascript'>alert('Username taken');</script>";
 }
+
+}
+
 ?>
