a689925ee436295bdfa564fee3a90bfc36a05dfc
annonymouse007 <43579453+@users.noreply.github.com>
2019-01-28 18:53:41 +0800

    Update createfile.php

--- includes/createfile.php
+++ includes/createfile.php
@@ -3,7 +3,7 @@
 if ($result){
      $query = "SELECT * FROM `users` WHERE username='$username'
 and password='".md5($password)."'";]
-        $result = mysqli_query($conn, $sql_query);
+        $result = mysqli_query($conn, $query);
         $row = mysqli_fetch_assoc($result);
         $id =  $row['ID'];
         mkdir ("uploads/incomeple/.$id",0777,true);
